96115bc08445d5cb1b101074ea59aede4a085483
Jason Gantenberg <jason.gantenberg@gmail.com>
2025-09-07 17:05:26 -0400

    Unlist the output object

--- R/parse.R
+++ R/parse.R
@@ -2,7 +2,7 @@
 #' @param edgestring A string specifying node-edge relationships
 #' @export
 parse_nodes <- function(edgestring) {
-  nodes <- stringr::str_extract_all(edgestring, stringr::boundary("word"))
+  nodes <- unlist(stringr::str_extract_all(edgestring, stringr::boundary("word")))
   nodes
 }
 
