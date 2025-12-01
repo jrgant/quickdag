c9d3bb47ee2e93e9b9a57236b5ada60da1885069
Jason Gantenberg <jason.gantenberg@gmail.com>
2025-12-01 00:11:22 -0500

    Add basic node and edge modification via alpha_ids
    Partially addresses #10

---
+++ R/graph_operations.R
@@ -0,0 +1,67 @@
+#' Graph operations
+#'
+#' @param graph_obj A `quickdag` object output by [qd_dag()] or [qd_swig()].
+#' @param alpha_ids A vector of alphanumeric node IDs upon which to operate.
+#' @param ... Pass other arguments to `DiagrammeR` functions:
+#'   [DiagrammeR::set_node_attrs()], [DiagrammeR::set_edge_attrs()].
+#' @param from_alpha A vector of alphanumeric source node IDs.
+#' @param to_alpha A vector of alphanumeric destination node IDs.
+#' @param set_op Passed to the `set_op` argument of [DiagrammeR::select_nodes_by_id()]
+#'   or [DiagrammeR::select_edges_by_node_id()]. Defaults to "union".
+#'
+#' @rdname graph_operations
+#' @export
+qd_set_node_attrs <- function(graph_obj, alpha_ids, ..., set_op = "union") {
+  numids <- get_numids(graph_obj, alpha_ids)
+  graph_out <- DiagrammeR::set_node_attrs(graph_obj, ..., nodes = numids)
+}
+
+#' @rdname graph_operations
+#' @export
+qd_set_edge_attrs <- function(graph_obj, ..., from_alpha = NULL, to_alpha = NULL,
+                              set_op = "union") {
+  from_numids <- NULL
+  to_numids <- NULL
+  if (!is.null(from_alpha)) {
+    from_numids <- get_numids(graph_obj, from_alpha)
+  }
+  if (!is.null(to_alpha)) {
+    to_numids <- get_numids(graph_obj, to_alpha)
+  }
+  graph_out <- DiagrammeR::set_edge_attrs(graph_obj, ...,
+                                          from = from_numids, to = to_numids)
+  graph_out
+}
+
+#' @rdname graph_operations
+#' @export
+select_nodes_by_alpha_id <- function(graph_obj, alpha_ids, set_op = "union") {
+  numids <- get_numids(graph_obj, alpha_ids)
+  graph_out <- DiagrammeR::select_nodes_by_id(graph_obj,
+                                              nodes = numids,
+                                              set_op = set_op)
+  graph_out
+}
+
+#' @rdname graph_operations
+#' @export
+qd_select_nodes <- select_nodes_by_alpha_id
+
+#' @rdname graph_operations
+#' @export
+select_edges_by_node_alpha_id <- function(graph_obj, alpha_ids, set_op = "union") {
+  numids <- get_numids(graph_obj, alpha_ids)
+  graph_out <- DiagrammeR::select_edges_by_node_id(graph_obj,
+                                                   nodes = numids,
+                                                   set_op = set_op)
+}
+
+#' @rdname graph_operations
+#' @export
+qd_select_edges <- select_edges_by_node_alpha_id
+
+get_numids <- function(graph_obj, alpha_ids) {
+  graph_obj$nodes_df |>
+    dplyr::filter(alpha_id %in% alpha_ids) |>
+    dplyr::pull(id)
+}
