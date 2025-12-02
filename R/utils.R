79722b4a8e6a8240ece9d93fb7973aa60ef83b88
Jason Gantenberg <jason.gantenberg@gmail.com>
2025-12-02 11:31:17 -0500

    Cosmetic

--- R/utils.R
+++ R/utils.R
@@ -1,14 +1,15 @@
 # Put some variables in the global environment to avoid R CMD CHECK notes
 utils::globalVariables(c("alpha_id", "label", "start", "end"))
 
+# Set global options
 .onLoad <- function(libname, pkgname) {
   opts <- list(
     quickdag.check_dag = TRUE,
-    quickdag.verbose = FALSE,
-    quickdag.format_special = FALSE,
+    quickdag.embed = FALSE,
     quickdag.fixed_sep = "vlin",
+    quickdag.format_special = FALSE,
     quickdag.theme = "base",
-    quickdag.embed = FALSE
+    quickdag.verbose = FALSE
   )
   options(opts)
 }
